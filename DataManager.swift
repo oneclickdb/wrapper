a01c17da023c8b5e8839e9cc5257f0b8a0d84a7b
oneclickdb <134239072+oneclickdb@users.noreply.github.com>
2026-05-12 08:38:05 +1000

    Update DataManager.swift

--- DataManager.swift
+++ DataManager.swift
@@ -13,12 +13,12 @@ class DataManager
 {
     //The connection to the database
     var db : OpaquePointer?
+    //MARK: Change the name of your database (.db) file to a name that relates to your project.  Use camel case with no spaces as shown in "nameYourDatabase.db".
     var fileName : String = "nameYourDatabase.db"
     let SQLITE_TRANSIENT = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
-    
+
     let tables: [String] = ["CREATE TABLE IF NOT EXISTS tableName(fieldText TEXT, fieldInteger INTEGER, fieldDouble REAL, fieldBool BOOL)"]
 
-    
     init()
     {
         Task
